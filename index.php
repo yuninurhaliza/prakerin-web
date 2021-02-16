957cd4b26daa236643c4de232d10b93c4f41c7c0
yuninurhaliza <77660310+yuninurhaliza@users.noreply.github.com>
2021-02-16 14:24:54 +0700

    Memvalidasi Password

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pesbuk</title>
+    <title>index</title>
 
-     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
+
 <body class="bg-light">
     <header>
         <div class="jumbotron jumbotron-fluid">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>Selamat datang di Pesbuk</h1>
+                        <h1>Selamat datang</h1>
                         <p>Bergabunglah bersama jutaan orang lainnya...</p>
                     </div>
                     <div class="col-md-4">
@@ -28,13 +30,14 @@
 
     <section>
         <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <img class="img img-responsive" src="img/connect.png" />
-                    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <img class="img img-responsive" src="img/connect.png" />
                 </div>
             </div>
+        </div>
     </section>
 
 </body>
-</html>   
+
+</html>
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 
 require_once("config.php");
 
-if(isset($_POST['register'])){
+if (isset($_POST['register'])) {
 
     // filter data yang diinputkan
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
@@ -30,66 +30,69 @@ if(isset($_POST['register'])){
 
     // jika query simpan berhasil, maka user sudah terdaftar
     // maka alihkan ke halaman login
-    if($saved) header("Location: login.php");
+    if ($saved) header("Location: login.php");
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register Pesbuk</title>
+    <title>Register</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
+
 <body class="bg-light">
 
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
 
-        <p>&larr; <a href="index.php">Home</a>
+                <p>&larr; <a href="index.php">Home</a>
 
-        <h4>Bergabunglah bersama ribuan orang lainnya...</h4>
-        <p>Sudah punya akun? <a href="login.php">Login di sini</a></p>
+                <h4>Bergabunglah bersama ribuan orang lainnya...</h4>
+                <p>Sudah punya akun? <a href="login.php">Login di sini</a></p>
 
-        <form action="" method="POST">
+                <form action="" method="POST">
 
-            <div class="form-group">
-                <label for="name">Nama Lengkap</label>
-                <input class="form-control" type="text" name="name" placeholder="Nama kamu" />
+                    <div class="form-group">
+                        <label for="name">Nama Lengkap</label>
+                        <input class="form-control" type="text" name="name" placeholder="Nama kamu" required />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input class="form-control" type="text" name="username" placeholder="Username" required />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input class="form-control" type="email" name="email" placeholder="Alamat Email" required />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input class="form-control" type="password" name="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Harus berisi setidaknya satu angka dan satu huruf besar dan kecil, dan setidaknya 8 karakter atau lebih" required />
+                    </div>
+
+                    <input type="submit" class="btn btn-success btn-block" name="register" value="Daftar" />
+
+                </form>
+
             </div>
 
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input class="form-control" type="text" name="username" placeholder="Username" />
+            <div class="col-md-6">
+                <img class="img img-responsive" src="img/connect.png" />
             </div>
 
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input class="form-control" type="email" name="email" placeholder="Alamat Email" />
-            </div>
-
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input class="form-control" type="password" name="password" placeholder="Password" />
-            </div>
-
-            <input type="submit" class="btn btn-success btn-block" name="register" value="Daftar" />
-
-        </form>
-            
         </div>
-
-        <div class="col-md-6">
-            <img class="img img-responsive" src="img/connect.png" />
-        </div>
-
     </div>
-</div>
 
 </body>
+
 </html>
